786ea869f67cac1b8697cba10986a2aeea3a3c77
Álex <alexglezfdez05@gmail.com>
2025-02-13 10:57:26 +0100

    preferences for arrow functions

--- src/app/client/modules.ts
+++ src/app/client/modules.ts
@@ -3,18 +3,18 @@ import { toDir, toFile } from '../utils/file.js'
 import type { loadedModules, Module } from '../types/modules.js'
 import type { Entity } from '../types/typeORM.js'
 
-async function loadModule(file: string): Promise<Module> {
+const loadModule = async (file: string): Promise<Module> => {
     const modulePath = toFile(`modules/${file}/mod.js`)
     const { default: module } = await import(modulePath)
     return module as Module
 }
 
-function processEntities(entity: Entity, moduleFile: string): Entity {
+const processEntities = (entity: Entity, moduleFile: string): Entity => {
     if (typeof entity !== 'string') return entity
     return toDir(`modules/${moduleFile}/${entity}`)
 }
 
-function processModule({ data, ...moduleData }: Module, file: string, result: loadedModules): void {
+const processModule = ({ data, ...moduleData }: Module, file: string, result: loadedModules): void => {
     if (data?.dbEntities) {
         const entities = Array.isArray(data.dbEntities) ? data.dbEntities : Object.values(data.dbEntities)
 
